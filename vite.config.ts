c2a0fbdb0550646bc2a36a97d81ae58f7b619353
zonghai <zonghai_zeng@163.com>
2025-02-25 19:42:56 +0100

    更新build输出目录

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(async ({ mode }) => {
     ],
     build: {
       minify: true,
-      outDir: 'docs',
+      outDir: 'build',
       sourcemap: false,
     },
     esbuild: {
